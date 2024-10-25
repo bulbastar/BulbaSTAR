f614e55a9480119ad5e6a54b43864ee3c7076797
Christina Bui <bulbairn@gmail.com>
2024-10-25 18:05:25 -0500

    Email

--- src/Components/Email.js
+++ src/Components/Email.js
@@ -8,10 +8,10 @@ function Email() {
         e.preventDefault(); // Prevent the default form submission
 
         try {
-            const response = await fetch('https://script.google.com/macros/s/AKfycbz3Z7Rh71zhgYjPE36xQWKSuhG0ehLFZWKrLpM6xDtGthBmO9cP_tL04i3LjPEjhmvX/exec', {
+            const response = await fetch('https://script.google.com/macros/s/AKfycbxURJfoC766tbC5MJcRQ37A8Oe5IJ1wEeOfKd5DjmWTMM8F6L-r384u3Sx2f0kqX300/exec', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded', // Use form-urlencoded
+                    'Content-Type': 'application/x-www-form-urlencoded',
                 },
                 body: new URLSearchParams({ email }), // Send email as form-encoded
             });
@@ -20,15 +20,15 @@ function Email() {
 
             if (response.ok) {
                 console.log('Email submitted successfully!');
-                setStatus({ success: true, error: false, message: 'Email submitted successfully!' }); // Set success message
+                setStatus({ success: true, error: false, message: 'Email submitted successfully!' });
                 setEmail(''); // Clear the input field
             } else {
-                console.error('Failed to submit email:', result); // Log the error
-                setStatus({ success: false, error: true, message: result }); // Set error message
+                console.error('Failed to submit email:', result);
+                setStatus({ success: false, error: true, message: result });
             }
         } catch (error) {
             console.error('Error:', error);
-            setStatus({ success: false, error: true, message: 'There was an error submitting your email. Please try again.' }); // Set error message
+            setStatus({ success: false, error: true, message: 'There was an error submitting your email. Please try again.' });
         }
     };
 
