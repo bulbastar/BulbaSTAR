e9ecbf90deb0217ee6a545300bfa9d8d6ebe0332
Christina Bui <bulbairn@gmail.com>
2024-10-25 16:33:31 -0500

    Fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+   
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
